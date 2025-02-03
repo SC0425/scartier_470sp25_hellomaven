df376b0d00ea95521f7ab061cda1d8ef94d9d406
SCARTIER <SCARTIER@BEUSCB4392.ds.sc.edu>
2025-02-03 12:35:06 -0500

    followed TDD principles to write first unit test

--- src/main/java/edu/uscb/csci470sp25/Calculator.java
+++ src/main/java/edu/uscb/csci470sp25/Calculator.java
@@ -2,6 +2,6 @@ package edu.uscb.csci470sp25;
 
 public class Calculator {
 	public int add(int a, int b) {
-		return 0;
-	}
+		return a + b;
+	} 
 }
--- src/main/java/edu/uscb/csci470sp25/HelloMaven.java
+++ src/main/java/edu/uscb/csci470sp25/HelloMaven.java
@@ -1,5 +1,7 @@
 package edu.uscb.csci470sp25;
 
+import java.util.Scanner;
+
 import org.apache.commons.lang3.StringUtils;
 
 public class HelloMaven {
@@ -7,5 +9,20 @@ public class HelloMaven {
 		String message = "   Hello, Maven     ";
 		System.out.println(" Original: " + message);
 		System.out.println(" Trimmed: " + StringUtils.trim(message));
+
+		Calculator myCalculator = new Calculator();
+
+		Scanner input = new Scanner(System.in); // NOTE: Will need to ...
+
+		System.out.print("Enter an integer: ");
+		int num1 = input.nextInt();
+
+		System.out.print("Enter another integer: ");
+		int num2 = input.nextInt();
+
+		System.out.println("The sum of " + num1 + " + " + num2 + " = " + myCalculator.add(num1, num2));
+
+		input.close(); // it's a good idea to close the Scanner object (may help ...)
+						// or use `...` to automatically close after use
 	}
 }
